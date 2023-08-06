8de338b5588af83d08facac82b0f537295d72e18
Ezerike Caleb Chikere <cezerike@gmail.com>
2023-08-06 20:44:52 +0100

    'implementing get requests for item ids'

--- backend/server.js
+++ backend/server.js
@@ -27,8 +27,8 @@ app.get('/api/categoryItems/tees', (req, res) => {
 })
 
 app.get('/api/categoryItems/tees/:id', (req, res) => {
-  const teesItem = teesItems.find((p) => p.linkName === req.params.id)
-  res.json(teesItem)
+  const tees = teesItems.find((p) => p.linkName === req.params.id)
+  res.json(tees)
 })
 
 app.get('/api/categoryItems/caps', (req, res) => {
@@ -43,23 +43,63 @@ app.get('/api/categoryItems/caps/:id', (req, res) => {
 app.get('/api/categoryItems/tshirts', (req, res) => {
   res.json(tshirtItems)
 })
+
+app.get('/api/categoryItems/tshirts/:id', (req, res) => {
+  const tshirt = tshirtItems.find((p) => p.linkName === req.params.id)
+  res.json(tshirt)
+})
+
 app.get('/api/categoryItems/hoodies', (req, res) => {
   res.json(hoodiesItems)
 })
+
+app.get('/api/categoryItems/hoodies/:id', (req, res) => {
+  const hoodie = hoodiesItems.find((p) => p.linkName === req.params.id)
+  res.json(hoodie)
+})
+
 app.get('/api/categoryItems/knitwears', (req, res) => {
   res.json(knitwearItems)
+
+  app.get('/api/categoryItems/knitwears/:id', (req, res) => {
+    const knitwear = knitwearItems.find((p) => p.linkName === req.params.id)
+    res.json(knitwear)
+  })
 })
 app.get('/api/categoryItems/footwears', (req, res) => {
   res.json(footwearItems)
 })
+
+app.get('/api/categoryItems/footwears/:id', (req, res) => {
+  const footwear = footwearItems.find((p) => p.linkName === req.params.id)
+  res.json(footwear)
+})
+
 app.get('/api/categoryItems/sets', (req, res) => {
   res.json(setsItems)
 })
+
+app.get('/api/categoryItems/sets/:id', (req, res) => {
+  const set = setsItems.find((p) => p.linkName === req.params.id)
+  res.json(set)
+})
+
 app.get('/api/categoryItems/pants', (req, res) => {
   res.json(pantsItems)
 })
+
+app.get('/api/categoryItems/pants/:id', (req, res) => {
+  const pant = pantsItems.find((p) => p.linkName === req.params.id)
+  res.json(pant)
+})
+
 app.get('/api/categoryItems/shorts', (req, res) => {
   res.json(shortItems)
+})
+
+app.get('/api/categoryItems/shorts/:id', (req, res) => {
+  const short = shortItems.find((p) => p.linkName === req.params.id)
+  res.json(short)
 })
 
 app.listen(5000, console.log('Server running on port 5000'))
