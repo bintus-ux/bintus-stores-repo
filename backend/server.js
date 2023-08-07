62e458369d35f8575d93a4d4d4421af6aa16f557
Ezerike Caleb Chikere <cezerike@gmail.com>
2023-08-07 14:38:25 +0100

    '.env variables'

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const categoryItems = require('./data/categoryItems')
 const {
   teesItems,
@@ -11,6 +12,8 @@ const {
   pantsItems,
   shortItems,
 } = require('./data/products')
+
+dotenv.config()
 
 const app = express()
 
@@ -102,4 +105,9 @@ app.get('/api/categoryItems/shorts/:id', (req, res) => {
   res.json(short)
 })
 
-app.listen(5000, console.log('Server running on port 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
